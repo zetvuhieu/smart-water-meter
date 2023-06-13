74d6e0403658402c16a9e671f2cd69500c80093f
Nguyen Vu Hieu <hieu.nv@besco.vn>
2023-06-13 10:32:28 +0700

    1306

--- src/pages/chart2.js
+++ src/pages/chart2.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import './chart2.css';
 
 const MonthlyChart = ({ data }) => {
     return (
-        <ResponsiveContainer width={1400} height={400}>
+        <ResponsiveContainer width={1400} height={400} className="monthly-chart-container">
             <LineChart data={data} margin={{ top: 5, right: 20, left: 20, bottom: 5 }}>
                 <XAxis dataKey="name" />
                 <YAxis />
